c05f289bee872594b1c44c886ad217b168fe6f73
nsbradford <nsbradford@gmail.com>
2015-04-14 22:16:00 -0400

    FINISHED: SixesWildApplication, ToMainScreenController

--- hackstreet/sixeswild/controller/ToMainScreenController.java
+++ hackstreet/sixeswild/controller/ToMainScreenController.java
@@ -1,5 +1,19 @@
 package hackstreet.sixeswild.controller;
 
-public class ToMainScreenController {
+import hackstreet.sixeswild.gui.SixesWildApplication;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+
+public class ToMainScreenController implements ActionListener{
+	SixesWildApplication application;
+	
+	public ToMainScreenController(SixesWildApplication application){
+		this.application = application;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e){
+		application.enterMainScreen();
+	}
 }
