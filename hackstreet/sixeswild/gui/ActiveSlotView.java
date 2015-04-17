397fa3a3e24561d9bbcbfc9eccd6ab0004b102ed
Benjamin Bianchi <benjaminbianchi@outlook.com>
2015-04-17 00:02:54 -0400

    idk

--- hackstreet/sixeswild/gui/ActiveSlotView.java
+++ hackstreet/sixeswild/gui/ActiveSlotView.java
@@ -1,5 +1,17 @@
 package hackstreet.sixeswild.gui;
 
-public class ActiveSlotView {
+import hackstreet.sixeswild.game.Slot;
+import hackstreet.sixeswild.level.AbstractLevel;
 
+import javax.swing.JButton;
+
+public class ActiveSlotView {
+	AbstractLevel level;
+	Slot slot;
+	JButton interactbtn;
+	
+	public ActiveSlotView(AbstractLevel abstractlevel, Slot slot )
+	{
+		
+	}
 }
