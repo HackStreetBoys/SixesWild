9d1c46ed6a726885a4d1f0a767118a922ac69e77
nsbradford <nsbradford@gmail.com>
2015-04-27 22:51:33 -0400

    Added Location, Slot, Tile, AbstractGameMove, and SwapTilesMove.

--- hackstreet/sixeswild/game/Tile.java
+++ hackstreet/sixeswild/game/Tile.java
@@ -1,10 +1,24 @@
 package hackstreet.sixeswild.game;
 
+/**
+ * A temporary placeholder for a Location's value and multiplier.
+ * 
+ * @author Nicholas
+ *
+ */
 public class Tile {
 
+	/** The value of the tile, {1-6}. */
 	private int value;
+	
+	/** The value of the multiplier, {1-3}. */
 	private int multiplier;
 	
+	/**
+	 * Tile constructor.
+	 * @param value The value of the tile, {1-6}.
+	 * @param multiplier The value of the multiplier, {1-3}.
+	 */
 	public Tile(int value, int multiplier){
 		this.value = value;
 		this.multiplier = multiplier;
--- hackstreet/sixeswild/move/AbstractGameMove.java
+++ hackstreet/sixeswild/move/AbstractGameMove.java
@@ -1,5 +1,22 @@
 package hackstreet.sixeswild.move;
 
-public abstract class AbstractGameMove implements IGameMove {
+import hackstreet.sixeswild.game.Slot;
 
+/**
+ * 
+ * 
+ * @author Nicholas
+ *
+ */
+public abstract class AbstractGameMove {
+	
+	/** List of selected Slots. */
+	ArrayList<Slot> selectedSlots;
+	
+	public AbstractGameMove(Slot selectedSlots[]){
+		this.selectedSlots = selectedSlots;
+	}
+	
+	public abstract boolean isValid();
+	public abstract void doMove();
 }
