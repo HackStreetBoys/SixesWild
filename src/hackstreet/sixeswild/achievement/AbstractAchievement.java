0efc1f6f7d235fbc39d614eb94418f16e86db5b0
Tim <tapetri@wpi.edu>
2015-04-29 23:17:00 -0400

    Added achievements, more to come

--- src/hackstreet/sixeswild/achievement/AbstractAchievement.java
+++ src/hackstreet/sixeswild/achievement/AbstractAchievement.java
@@ -1,5 +1,48 @@
 package hackstreet.sixeswild.achievement;
 
-public class AbstractAchievement {
+import hackstreet.sixeswild.SixesWild;
 
+import java.util.Date;
+
+public abstract class AbstractAchievement {
+	protected String name;
+	protected boolean achieved;
+	protected Date dateAchieved;
+	
+	public abstract boolean isAchieved(SixesWild model);
+	
+	public AbstractAchievement(boolean achieved, Date dateAchieved) {
+		// names are set in subclasses
+		this.achieved = achieved;
+		this.dateAchieved = dateAchieved;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String s) {
+		this.name = s;
+	}
+	
+	public boolean getAchieved() {
+		return this.achieved;
+	}
+	
+	public void setAchieved(boolean b) {
+		this.achieved = b;
+	}
+	
+	public Date getDateAchieved() {
+		return this.dateAchieved;
+	}
+	
+	public void setDateAchieved(Date d) {
+		this.dateAchieved = d;
+	}
+	
+	
+	
+	
+	
 }
--- src/hackstreet/sixeswild/achievement/AchievementAllLevelsCompleted.java
+++ src/hackstreet/sixeswild/achievement/AchievementAllLevelsCompleted.java
@@ -1,5 +1,43 @@
 package hackstreet.sixeswild.achievement;
 
-public class AchievementAllLevelsCompleted {
+import hackstreet.sixeswild.SixesWild;
+import hackstreet.sixeswild.config.SavedLevelData;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AchievementAllLevelsCompleted extends AbstractAchievement {
+	
+	
+	
+	public AchievementAllLevelsCompleted(boolean achieved, Date dateAchieved) {
+		super(achieved, dateAchieved);
+		this.name = "All levels competed";
+	}
+
+
+	@Override
+	public boolean isAchieved(SixesWild model) {
+		
+		ArrayList<SavedLevelData> list = model.getSavedLevelDataList();
+		
+		for (SavedLevelData s: list) {
+			if (s.getStarsEarned() == 0) {
+				return false;
+			}
+		}
+		
+		this.achieved = true;
+		this.dateAchieved = new Date();
+		
+		return this.achieved;
+
+	}
+	
+	
+	
+	
+	
+	
+	
 }
